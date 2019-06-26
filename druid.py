3840a1af5c5b47ab0e1ff3c4e0196353c40017ce
Isaac Pearl <pearl.isaac@gmail.com>
2019-06-25 20:33:25 -0400

    initial upload with sketchpad!

--- druid.py
+++ druid.py
@@ -2,6 +2,14 @@ import sys
 import serial
 import readline
 
+def getLua():
+    script = ""
+    with open("./sketch.lua") as d:
+        lua = d.readlines()
+        for line in lua:
+            script += line
+    return script
+
 try:
   ser = serial.Serial("/dev/ttyACM0",115200, timeout=0.1)
 except:
@@ -13,6 +21,14 @@ cmd = ""
 print("//// druid. q to quit.")
 
 while cmd != "q":
-  ser.write(cmd+"\r\n")
+  if cmd == "r":
+    ser.write(getLua())
+  elif cmd == "u":
+    ser.write("^^s")
+    ser.write(getLua())
+    ser.write("^^e")
+  else:
+    ser.write(cmd+"\r\n")
   print(ser.read(1000000))
   cmd = raw_input("> ")
+
---
+++ sketch.lua
@@ -0,0 +1,3 @@
+debug_usart("hi")
+output[2].volts = 4.0
+print(input[1]())
