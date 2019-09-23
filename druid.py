aea35aae8f944d70e96d21862739877f937f24ee
Trent <trent.gill@gmail.com>
2019-09-23 17:33:04 -0400

    refactor for cleaner mainloop & file-passing from within druid & cl provided runtime execution

--- druid.py
+++ druid.py
@@ -5,12 +5,7 @@ try:
     import readline
 except:
     print("readline failed to import")
-
-def forLuaLines( fn, file ):
-    with open(file) as d:
-        lua = d.readlines()
-        for line in lua:
-            fn( line )
+import time
 
 port = ""
 for item in serial.tools.list_ports.comports():
@@ -27,32 +22,56 @@ except:
   print("can't open serial port")
   exit()
 
-cmd = ""
+def forLuaLines( fn, file ):
+    with open(file) as d:
+        lua = d.readlines()
+        for line in lua:
+            fn( line )
 
+def uploader( fn, file ):
+    print(" uploading "+file)
+    fn("^^k")
+    time.sleep(0.4) # wait for restart
+    fn("^^s")
+    time.sleep(0.2) # wait for allocation
+    forLuaLines( fn, file )
+    time.sleep(0.2) # wait for upload to complete
+    fn("^^e")
+
+def runner( fn, file ):
+    print(" running "+file)
+    fn("```")
+    forLuaLines( fn, file )
+    time.sleep(0.1)
+    fn("```")
+
+# enter
 print("//// druid. q to quit.")
 
-import time
+# run script passed from command line
+if len(sys.argv) == 2:
+    runner( ser.write, sys.argv[1] )
 
+# repl
+cmd = ""
 while cmd != "q":
-  if cmd == "r":
-    ser.write("```")
-    forLuaLines( ser.write, "./sketch.lua" )
-    time.sleep(0.1)
-    ser.write("```")
-  elif cmd == "u":
-    ser.write("^^k")
-    time.sleep(0.4) # wait for restart
-    ser.write("^^s")
-    time.sleep(0.2) # wait for allocation
-    forLuaLines( ser.write, "./sketch.lua" )
-    time.sleep(0.2) # wait for upload to complete
-    ser.write("^^e")
-  elif cmd == "p":
-    ser.write("^^p")
-  else:
-    ser.write(cmd+"\r\n")
-  print(ser.read(1000000))
-  cmd = raw_input("> ")
+    if "r " in cmd:
+        runner( ser.write, cmd[2:] )
+    elif cmd == "r":
+        runner( ser.write, "./sketch.lua" )
+    elif "u " in cmd:
+        uploader( ser.write, cmd[2:] )
+    elif cmd == "u":
+        uploader( ser.write, "./sketch.lua" )
+    elif cmd == "p":
+        ser.write("^^p")
+    elif cmd == "help":
+        print(druid_help)
+    else:
+        ser.write(cmd+"\r\n")
+    print(ser.read(1000000))
+    cmd = raw_input("> ")
 
+# leave
 ser.close()
 exit()
