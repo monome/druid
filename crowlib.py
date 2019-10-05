c33bc5f4cec300afce6fdba991037cd9f328eaba
Sam Boling <charles.samuel.boling@gmail.com>
2019-10-04 21:48:17 -0600

    requirements + crowlib logging + pep8 formatting

--- crowlib.py
+++ crowlib.py
@@ -1,46 +1,58 @@
+import logging
 import serial
 import serial.tools.list_ports
 import time
 
+logger = logging.getLogger(__name__)
+
+
 def connect():
     port = ""
     for item in serial.tools.list_ports.comports():
+        logger.info("comport {} - device {}".format(item[0], item[2]))
         if "USB VID:PID=0483:5740" in item[2]:
             port = item[0]
+            logger.info('using {}'.format(port))
     if port == "":
+        logger.error("crow not found, exit")
         raise ValueError("can't find crow device")
     try:
-        return serial.Serial(port,115200, timeout=0.1)
-    except:
+        return serial.Serial(port, 115200, timeout=0.1)
+    except serial.SerialException as e:
+        logger.error("could not open comport {}".format(port), e)
         raise ValueError("can't open serial port")
+
 
 def reconnect():
     try:
         c = crow_connect()
-        myprint( " <online!>" )
+        myprint(" <online!>")
         return c
     except ValueError as err:
-        myprint( " <lost connection>" )
+        myprint(" <lost connection>")
 
-def writelines( writer, file ):
+
+def writelines(writer, file):
     with open(file) as d:
         lua = d.readlines()
         for line in lua:
-            writer( line.encode() ) # convert text to bytes
+            writer(line.encode())  # convert text to bytes
 
-def upload( writer, printer, file ):
+
+def upload(writer, printer, file):
     printer(" uploading "+file)
     writer(bytes("^^k", 'utf-8'))
-    time.sleep(0.4) # wait for restart
+    time.sleep(0.4)  # wait for restart
     writer(bytes("^^s", 'utf-8'))
-    time.sleep(0.2) # wait for allocation
-    writelines( writer, file )
-    time.sleep(0.2) # wait for upload to complete
+    time.sleep(0.2)  # wait for allocation
+    writelines(writer, file)
+    time.sleep(0.2)  # wait for upload to complete
     writer(bytes("^^e", 'utf-8'))
 
-def execute( writer, printer, file ):
+
+def execute(writer, printer, file):
     printer(" running "+file+"\r")
     writer(bytes("```", 'utf-8'))
-    writelines( writer, file )
+    writelines(writer, file)
     time.sleep(0.1)
     writer(bytes("```", 'utf-8'))
---
+++ setup.sh
@@ -0,0 +1,6 @@
+python -m venv .
+source Scripts/activate
+pip install -r requirements.txt
+echo 'ready. you may need to run:'
+echo '  source Scripts/activate'
+echo 'in this folder again after opening a new shell'
