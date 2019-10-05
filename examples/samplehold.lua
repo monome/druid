a13a6653d778c43e830bdbb5dc4b9501551bcf33
trentgill <trent.gill@gmail.com>
2019-10-05 00:51:05 -0400

    some examples!

---
+++ examples/samplehold.lua
@@ -0,0 +1,26 @@
+--- sample & hold
+-- in1: input voltage
+-- in2: sampling clock
+-- out1: s&h input
+-- out2: s&h input (quantized chromatically)
+-- out3: s&h random
+-- out4: s&h input+random nudge (quantized chromatically)
+
+function init()
+    input[1].mode = 'none'
+    input[2]{ mode      = 'change'
+            , direction = 'rising'
+            }
+    for n=1,4 do output[n].slew = 0 end
+end
+
+input[2].change = function()
+    v = input[1].volts
+    output[1].volts = v
+    output[2].volts = math.floor(v*12)/12
+    r = math.random() * 10.0 - 5.0
+    output[3].volts = r
+    output[4].volts = math.floor(r + v*12)/12
+end
+
+init()
--- examples/test-2.lua
+++
@@ -1,5 +0,0 @@
-x=1
-input[1].mode('stream',0.01)
-input[1].stream = function(y)
-  output[1].volts = y
-end
