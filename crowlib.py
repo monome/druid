9a1df47511f3b2613765ff47aed1e8373dc165fc
Trent <trent.gill@gmail.com>
2019-10-07 13:18:51 -0400

    move to 1.0 style bulk run/flash messages & increased delay times for stability

--- crowlib.py
+++ crowlib.py
@@ -27,25 +27,30 @@ def writelines( writer, file ):
         lua = d.readlines()
         for line in lua:
             writer( line.encode() ) # convert text to bytes
-            time.sleep(0.001) # fix os x crash?
+            time.sleep(0.002) # fix os x crash?
 
 def upload( writer, printer, file ):
     printer(" uploading "+file+"\n\r")
-    writer(bytes("^^k", 'utf-8'))
-    time.sleep(0.4) # wait for restart
     writer(bytes("^^s", 'utf-8'))
     time.sleep(0.2) # wait for allocation
     writelines( writer, file )
-    time.sleep(0.2) # wait for upload to complete
-    writer(bytes("^^e", 'utf-8'))
-    time.sleep(0.2) # wait for flash write
-    writer(bytes("init()\n\r", 'utf-8'))
+    time.sleep(0.4) # wait for upload to complete
+    writer(bytes("^^w", 'utf-8'))
 
 def execute( writer, printer, file ):
     printer(" running "+file+"\n\r")
-    writer(bytes("```", 'utf-8'))
+    writer(bytes("^^s", 'utf-8'))
+    time.sleep(0.2) # wait for allocation
     writelines( writer, file )
-    time.sleep(0.1)
-    writer(bytes("```", 'utf-8'))
-    time.sleep(0.1)
-    writer(bytes("init()\n\r", 'utf-8'))
+    time.sleep(0.2)
+    writer(bytes("^^e", 'utf-8'))
+
+# leaving this here for 'run a code chunk'
+#def execute( writer, printer, file ):
+#    printer(" running "+file+"\n\r")
+#    writer(bytes("```", 'utf-8'))
+#    writelines( writer, file )
+#    time.sleep(0.1)
+#    writer(bytes("```", 'utf-8'))
+#    time.sleep(0.1)
+#    writer(bytes("init()\n\r", 'utf-8'))
