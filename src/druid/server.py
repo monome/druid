c45dcade5fe62875967b5a8f6a572c72d68043db
Sam Boling <charles.samuel.boling@gmail.com>
2021-06-21 19:55:47 -0600

    druid messages for websocket conn/disconn/crash

--- src/druid/server.py
+++ src/druid/server.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+from websockets.exceptions import ConnectionClosedError
 
 
 class DruidServer:
@@ -10,14 +11,21 @@ class DruidServer:
         self.port = port
 
     async def handle(self, websocket, path):
+        (host, *args) = websocket.remote_address
+        self.repl.output(f'\n <ws connected: {host}>')
         self.repl.handlers['crow_output'].append(
             lambda output: asyncio.ensure_future(
                 self.handle_output(websocket, output)
             )
         )
-        async for message in websocket:
-            self.repl.output(f'\n> {message}\n')
-            self.repl.crow.writeline(message)
+        try:
+            async for message in websocket:
+                self.repl.output(f'\n> {message}\n')
+                self.repl.crow.writeline(message)
+        except ConnectionClosedError as e:
+            self.repl.output(f'\n <ws disconnected: {host} ({e.code} {e.reason or "no reason"})>')
+        else:
+            self.repl.output(f'\n <ws disconnected: {host}>')
 
     async def listen(self):
         self.repl.output(f' <listening at ws://{self.host}:{self.port}>')
